62af1c5c518d6acf9cfeeaaa66328ac5057aab01
Erica Kirkbride <ericakirkbride@gmail.com>
2021-07-13 12:07:11 -0600

    Change package name to pyTQA for full import

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setuptools.setup(
     author_email="whitaker@imageowl.com",
     name='pyTQA',
     description='Python Wrapper for the Image Owl Total QA',
-    version='1.0.2',
+    version='1.0.4',
     long_description=README,
     url='https://github.com/imageowl/pyTQA.git',
-    packages=setuptools.find_packages(),
+    packages=['pyTQA'],
     python_requires=">=3.6",
     install_requires=['requests', 'python-dateutil'],
     classifiers=[

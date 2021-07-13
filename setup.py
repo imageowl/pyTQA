581c264c8982479424e4cee9c7b0e5b3be7169df
Erica Kirkbride <ericakirkbride@gmail.com>
2021-07-13 10:47:58 -0600

    Newly updated build

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setuptools.setup(
     author_email="whitaker@imageowl.com",
     name='pyTQA',
     description='Python Wrapper for the Image Owl Total QA',
-    version='v1.0.0',
+    version='1.0.2',
     long_description=README,
     url='https://github.com/imageowl/pyTQA.git',
     packages=setuptools.find_packages(),
-    python_requires=">=3.8.5",
+    python_requires=">=3.6",
     install_requires=['requests', 'python-dateutil'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.6',
     ],
 )
